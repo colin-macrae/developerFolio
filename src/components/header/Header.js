7ee9c96424e12cd0699fcc3466a3f1ffe9e3e8dc
colin-macrae <csmacrae@gmail.com>
2025-02-02 16:47:55 -0800

    comment out unused imports

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,21 +7,21 @@ import {
   greeting,
   workExperiences,
   skillsSection,
-  openSource,
-  blogSection,
-  talkSection,
-  achievementSection,
+  // openSource,
+  // blogSection,
+  // talkSection,
+  // achievementSection,
   resumeSection
 } from "../../portfolio";
 
 function Header() {
   const {isDark} = useContext(StyleContext);
   const viewExperience = workExperiences.display;
-  const viewOpenSource = openSource.display;
+  // const viewOpenSource = openSource.display;
   const viewSkills = skillsSection.display;
-  const viewAchievement = achievementSection.display;
-  const viewBlog = blogSection.display;
-  const viewTalks = talkSection.display;
+  // const viewAchievement = achievementSection.display;
+  // const viewBlog = blogSection.display;
+  // const viewTalks = talkSection.display;
   const viewResume = resumeSection.display;
 
   return (
